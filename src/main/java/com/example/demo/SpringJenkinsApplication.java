f72e6a49b317e129441add1cd1eec6d8da3ffd03
barisbicer <baris_bicer@hotmail.com>
2021-07-06 10:11:06 +0300

    second commit

--- src/main/java/com/example/demo/SpringJenkinsApplication.java
+++ src/main/java/com/example/demo/SpringJenkinsApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import javax.annotation.PostConstruct;
+
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +11,13 @@ import ch.qos.logback.classic.Logger;
 @SpringBootApplication
 public class SpringJenkinsApplication {
 	
-	Logger logger=(Logger) LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	public static Logger logger=(Logger) LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		
+		logger.info("Aplication started.....");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJenkinsApplication.class, args);
